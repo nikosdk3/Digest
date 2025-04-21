6984d2208d9b5e5d6490d2eb038c0267d7054581
Nikos <nikomvp03@gmail.com>
2025-04-21 01:32:00 -0500

    Build for macOS

--- apps/digest/src/components/ReaderContent.tsx
+++ apps/digest/src/components/ReaderContent.tsx
@@ -2,34 +2,61 @@
 
 import * as React from 'react';
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+
+import { useEnv } from '@/context/EnvContext';
+import { useReaderStore } from '@/store/readerStore';
 import { BookDoc, DocumentLoader } from '@/libs/document';
-import { BookContent } from '@/types/book';
+
 import FoliateViewer from './FoliateViewer';
+import Spinner from './Spinner';
 
-interface ReaderContentProps {
-  content: BookContent;
-}
+const ReaderContent = () => {
+  const searchParams = useSearchParams();
+  const id = searchParams.get('id');
 
-const ReaderContent: React.FC<ReaderContentProps> = ({ content }) => {
   const [bookDoc, setBookDoc] = useState<BookDoc>();
+  const { envConfig } = useEnv();
+  const { books, fetchBook } = useReaderStore();
+  const bookState = books[id!] || { loading: true, content: null, error: null };
 
   useEffect(() => {
+    if (id && !bookState.content) {
+      envConfig.appService().then((appService) => {
+        appService.loadSettings().then(() => {
+          fetchBook(appService, id).then((book) => {
+            if (book) {
+              book.lastUpdated = Date.now();
+              appService.updateLibraryBook(book);
+            }
+          });
+        });
+      });
+    }
     const loadDocument = async () => {
-      if (content.file) {
+      const content = bookState.content;
+      if (content) {
         const { book } = await new DocumentLoader(content.file).open();
         setBookDoc(book);
       }
     };
+    if (bookState.content) {
+      loadDocument();
+    }
+  }, [bookState.content, envConfig, fetchBook, id]);
 
-    loadDocument();
-  }, [content.file]);
-
-  if (!content.file || !bookDoc) {
+  if (!bookDoc) {
     return null;
   }
 
   return (
     <div>
+      {bookState.loading && <Spinner loading={bookState.loading} />}
+      {bookState.error && (
+        <div className='text-center'>
+          <h2 className='text-red-500'>{bookState.error}</h2>
+        </div>
+      )}
       <FoliateViewer book={bookDoc} />
     </div>
   );
